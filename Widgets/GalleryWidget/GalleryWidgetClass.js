7a15943e46e76659c32f0af908ebd40eebcb202a
meustrasbourg <55553891+meustrasbourg@users.noreply.github.com>
2019-10-05 13:57:03 +0200

    Delete GalleryWidgetClass.js

--- Widgets/GalleryWidget/GalleryWidgetClass.js
+++
@@ -1,9 +0,0 @@
-import * as Scrivito from "scrivito";
-
-const GalleryWidget = Scrivito.provideWidgetClass("GalleryWidget", {
-  attributes: {
-    images: ["referencelist", { only: ["Image"] }],
-  },
-});
-
-export default GalleryWidget;
