336932f31574270e18c9c150d7f3ae2e4ec284db
meustrasbourg <directorate@meu-strasbourg.org>
2019-10-05 14:55:48 +0200

    Delete unnecessary folders and files

--- Widgets/SpaceWidget/SpaceWidgetClass.js
+++
@@ -1,9 +0,0 @@
-import * as Scrivito from "scrivito";
-
-const SpaceWidget = Scrivito.provideWidgetClass("SpaceWidget", {
-  attributes: {
-    size: "float",
-  },
-});
-
-export default SpaceWidget;
--- Widgets/SpaceWidget/SpaceWidgetComponent.js
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import * as Scrivito from "scrivito";
-
-Scrivito.provideComponent("SpaceWidget", ({ widget }) => {
-  const size = widget.get("size") || 5;
-  return <div style={{ height: `${size}em` }} />;
-});
