3c84af6758c8d188c24edc96203f2bc40a81c310
meustrasbourg <55553891+meustrasbourg@users.noreply.github.com>
2019-10-05 13:53:25 +0200

    Delete DividerWidgetClass.js

--- Widgets/DividerWidget/DividerWidgetClass.js
+++
@@ -1,9 +0,0 @@
-import * as Scrivito from "scrivito";
-
-const DividerWidget = Scrivito.provideWidgetClass("DividerWidget", {
-  attributes: {
-    showLogo: ["enum", { values: ["yes", "no"] }],
-  },
-});
-
-export default DividerWidget;
