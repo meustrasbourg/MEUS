b6a8784a78928b79e76dbe4ef42907c021b8929b
meustrasbourg <55553891+meustrasbourg@users.noreply.github.com>
2019-10-05 12:12:51 +0200

    Update index.js

--- src/Widgets/index.js
+++ src/Widgets/index.js
@@ -4,5 +4,3 @@ function importAll(r) {
 
 // import all js files under src/Widgets/
 importAll(require.context("./", true, /\.js$/));
-import "scrivito-collapsible-card";
-import "scrivito-collapsible-card/index.css";
